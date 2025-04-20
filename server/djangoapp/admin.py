d239615497709c108e0edd0e9e001bb6703d9175
Nehal Hamdy <nehalhamdy41@gmail.com>
2025-04-20 07:29:19 -0400

    Adding temporary changes to Github

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,29 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 3  # Shows 3 empty slots for adding new models
+    show_change_link = True  # Allows clicking through to edit individual models
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'dealer_id')
+    list_filter = ('car_make', 'type', 'year')
+    search_fields = ('name', 'car_make__name', 'type')
+    list_select_related = ('car_make',)  # Optimizes database queries
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'founded_year', 'country')
+    search_fields = ('name', 'country', 'description')
+    list_filter = ('founded_year', 'country')
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,7 +12,19 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    founded_year = models.IntegerField(null=True, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+    logo = models.ImageField(upload_to='make_logos/', null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Car Make"
+        verbose_name_plural = "Car Makes"
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -23,3 +35,32 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('TRUCK', 'Truck'),
+        ('VAN', 'Van'),
+        ('HATCHBACK', 'Hatchback'),
+    ]
+
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()  # Reference to dealer in Cloudant
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')
+    year = models.DateField()
+    engine = models.CharField(max_length=50, blank=True)
+    trim_level = models.CharField(max_length=50, blank=True)
+    color = models.CharField(max_length=50, blank=True)
+    mileage = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year.year})"
+
+    class Meta:
+        verbose_name = "Car Model"
+        verbose_name_plural = "Car Models"
+        
